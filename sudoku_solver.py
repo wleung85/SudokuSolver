9aebfd925281aad2d2218b9ea15adb193da2b077
Winston <wleung85@uwo.ca>
2020-01-11 15:27:11 -0500

    Added creating puzzle with values

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,7 +5,7 @@
 class Square:
     def __init__(self, value=0, solved=False):
         self.possibles = []     # possible values
-        self._value = value
+        self.value = value
         self.solved = solved
 
     @property
@@ -26,24 +26,22 @@ class Square:
 # This represents the entire puzzle with a 9x9 grid of Squares
 class Puzzle:
     def __init__(self, given_values=None):
+        self.grid = []
         # Type check to make sure given values is a 2D array with lengths of 9x9
         if given_values != None:
             if not isinstance(given_values, list) or len(given_values) != 9:
                 raise ValueError("Puzzle must be initialized with a 9x9 2D sarray")
-            else:
-                for i in range(9):
-                    if len(given_values[i]) != 9:
-                        raise ValueError("Puzzle must be initialized with a 9x9 2D array")
-
-        # Create grid with empty Squares
-        if given_values == None:
-            self.grid = []
-            for i in range(9):
-                row = []
-                for j in range(9):
-                    row.append(Square())
-                self.grid.append(row)
-        # TODO: fill grid according to given values, could combine with previous grid creation
+            for row in given_values:
+                if len(row) != 9:
+                    raise ValueError("Puzzle must be initialized with a 9x9 2D sarray")
+        
+        # Fill grid with Squares
+        for i in range(9):
+            row = []
+            for j in range(9):
+                value = 0 if given_values is None else given_values[i][j]
+                row.append(Square(value))
+            self.grid.append(row)
 
     def __str__(self):
         # Method to give a string representation of what the puzzle looks like
@@ -66,8 +64,15 @@ class Puzzle:
 
 if __name__ == "__main__":
     # Create an empty puzzle with no squares filled
+    print("Creating empty puzzle")
     empty_puzzle = Puzzle()
+    print (empty_puzzle)
+
+    # Create a Puzzle with values
+    print("Creating filled puzzle")
+    input_arr = []
     for i in range(9):
-        for j in range(9):
-            empty_puzzle.grid[i][j].value = j + 1
-    print(empty_puzzle)
+        row = [i for i in range(1, 10)]
+        input_arr.append(row)
+    filled_puzzle = Puzzle(input_arr)
+    print(filled_puzzle)
