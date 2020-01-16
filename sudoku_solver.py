5b47e228b09fe8de48d8f91d08daffc996e8f965
Winston <wleung85@uwo.ca>
2020-01-16 00:18:38 -0500

    Corrected getting subgrid method and created method to iterate through rows, columns, and subgrid to check if Puzzle is solved

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,10 +5,17 @@ PUZ_HEIGHT = 9
 
 # This represents one index/square with one value
 class Square:
-    def __init__(self, value=0, solved=False):
+    def __init__(self, value=0):
         self.possibles = []     # possible values
         self.value = value
-        self.solved = solved
+        
+        # Square is already solved and given if initialized with a non-zero/blank
+        if value:
+            self.solved = True
+            self.given = True
+        else:
+            self.solved = False
+            self.given = False
 
     @property
     def value(self):
@@ -17,7 +24,7 @@ class Square:
     @value.setter
     def value(self, value):
         # Range check value
-        if value < 0 or value > 9:
+        if not isinstance(value, int) or value < 0 or value > 9:
             raise ValueError("Square must have value 1-9 or 0 to indicate no value")
         self._value = value
         if value == 0:
@@ -31,14 +38,15 @@ class Square:
 # This represents the entire puzzle with a 9x9 grid of Squares
 class Puzzle:
     def __init__(self, given_values=None):
+        self.solved = False
         self.grid = []
         # Type check to make sure given values is a 2D array with lengths of 9x9
         if given_values != None:
             if not isinstance(given_values, list) or len(given_values) != PUZ_HEIGHT:
-                raise ValueError("Puzzle must be initialized with a 9x9 2D sarray")
+                raise ValueError("Puzzle must be initialized with a 9x9 2D array")
             for row in given_values:
                 if len(row) != PUZ_HEIGHT:
-                    raise ValueError("Puzzle must be initialized with a 9x9 2D sarray")
+                    raise ValueError("Puzzle must be initialized with a 9x9 2D array")
         
         # Fill grid with Squares
         for i in range(PUZ_HEIGHT):
@@ -47,6 +55,46 @@ class Puzzle:
                 value = 0 if given_values is None else given_values[i][j]
                 row.append(Square(value))
             self.grid.append(row)
+
+    # Returns True if Puzzle is solved, else False
+    def is_solved(self):
+        for row in range(PUZ_HEIGHT):
+            lst = self.get_row(row)
+            if not self.sqlist_is_solved(lst):
+                return False
+
+        for col in range(PUZ_WIDTH):
+            lst = self.get_col(col)
+            if not self.sqlist_is_solved(lst):
+                return False
+
+        for row in range(PUZ_HEIGHT // 3):
+            for col in range(PUZ_WIDTH // 3):
+                lst = self.get_subgrid(row * 3, col * 3)
+                if not self.sqlist_is_solved(lst):
+                    return False
+
+        return True
+            
+    
+    @staticmethod
+    # Checks if list of squares is solved (i.e. contains values 1-9)
+    def sqlist_is_solved(lst):
+        solved = True
+        if len(lst) != 9:
+            solved = False
+        else:
+            for value in range(1, 10):
+                value_found = False
+                for square in lst:
+                    if square.value == value:
+                        value_found = True
+                        break
+                if not value_found:
+                    solved = False
+                    break
+
+        return solved
 
     # Gets Square object using row and column index
     def get_square(self, row, col):
@@ -88,10 +136,10 @@ class Puzzle:
         else:
             raise ValueError("Method expects either a single Square argument or a row and column argument")
 
-        # Get subgrid coordinates (top left 3x3 subgrid is (0,0) for example)
+        # Find first index (top-left square) of subgrid
+        row = row // 3 * 3
+        col = col // 3 * 3
         result = []
-        row = row // 3
-        col = col // 3
         for row_iter in range(PUZ_HEIGHT // 3):
             for col_iter in range(PUZ_WIDTH // 3):
                 result.append(self.grid[row + row_iter][col + col_iter])
@@ -160,5 +208,33 @@ if __name__ == "__main__":
     subgrid = filled_puzzle.get_subgrid(2, 2)
     for square in subgrid:
         print(square)
+
+    print("-" * 20)
+    print("Check if list of squares is solved")
+    lst = []
+    for value in range(1, 10):
+        lst.append(Square(value))
+    print(Puzzle.sqlist_is_solved(lst))
+    lst[0].value = 4
+    print(Puzzle.sqlist_is_solved(lst))
+
+    print("-" * 20)
+    # print("Check if filled puzzle is solved")
+    # print(filled_puzzle.is_solved())
+    print("Check if solved puzzle is solved")
+    input_arr = [[8, 2, 7, 1, 5, 4, 3, 9, 6],\
+                 [9, 6, 5, 3, 2, 7, 1, 4, 8],\
+                 [3, 4, 1, 6, 8, 9, 7, 5, 2],\
+                 [5, 9, 3, 4, 6, 8, 2, 7, 1],\
+                 [4, 7, 2, 5, 1, 3, 6, 8, 9],\
+                 [6, 1, 8, 9, 7, 2, 4, 3, 5],\
+                 [7, 8, 6, 2, 3, 5, 9, 1, 4],\
+                 [1, 5, 4, 7, 9, 6, 8, 2, 3],\
+                 [2, 3, 9, 8, 4, 1, 5, 6, 7]]
+    solved_puzzle = Puzzle(input_arr)
+    print(solved_puzzle)
+    print(solved_puzzle.is_solved())
+
+    # TODO: solved_puzzle should return solved, getting subgrid seems broken
 
     print("Finished run")
