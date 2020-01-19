bd8e47ddf8f9219a8bea3fda5cfd2a5847368d17
Winston <wleung85@uwo.ca>
2020-01-18 21:32:43 -0500

    Added medium and hard puzzle examples to debug

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -338,4 +338,32 @@ if __name__ == "__main__":
     #     print(easy_puzzle)
     easy_puzzle.solve(verbose=True)
 
+    print("-" * 20)
+    print("Evaluate solving medium puzzle")
+    input_arr = [[5, 0, 7, 0, 9, 0, 3, 0, 0],\
+                 [3, 0, 8, 0, 7, 2, 0, 0, 0],\
+                 [2, 1, 0, 0, 0, 3, 0, 0, 0],\
+                 [0, 2, 0, 0, 0, 7, 0, 0, 1],\
+                 [7, 0, 0, 0, 1, 0, 0, 2, 8],\
+                 [4, 3, 0, 2, 0, 0, 0, 0, 0],\
+                 [0, 0, 0, 3, 0, 0, 0, 8, 9],\
+                 [0, 0, 0, 1, 4, 0, 2, 0, 7],\
+                 [0, 0, 3, 0, 8, 0, 1, 0, 6]]
+    med_puzzle = Puzzle(input_arr)
+    med_puzzle.solve(verbose=True)
+
+    print("-" * 20)
+    print("Evaluate solving hard puzzle")
+    input_arr = [[0, 3, 0, 0, 0, 5, 0, 0, 0],\
+                 [4, 0, 0, 6, 0, 0, 7, 1, 3],\
+                 [6, 0, 0, 0, 0, 0, 0, 9, 0],\
+                 [7, 0, 0, 0, 2, 0, 0, 0, 0],\
+                 [0, 1, 0, 4, 6, 9, 0, 8, 0],\
+                 [0, 0, 0, 0, 7, 0, 0, 0, 6],\
+                 [0, 8, 0, 0, 0, 0, 0, 0, 2],\
+                 [2, 4, 9, 0, 0, 6, 0, 0, 5],\
+                 [0, 0, 0, 9, 0, 0, 0, 6, 0]]
+    hard_puzzle = Puzzle(input_arr)
+    hard_puzzle.solve(verbose=True)
+
     print("Finished run")
